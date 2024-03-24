2121380714af2dbbe0af2da8d972e060c89f4f2a
rodrigo.santos <rodrigoos@dcomp.ufs.br>
2024-03-24 13:50:03 -0300

    exemplo estático de candidatos

--- Beth bee/resources/views/candidatos.blade.php
+++ Beth bee/resources/views/candidatos.blade.php
@@ -8,6 +8,200 @@
 
 @section('content')
     <p>Busca por Candidato</p>
+    <div class="d-flex flex-wrap justify-content-around">
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+        <div class="card" style="width: 18rem;">
+            <img src="{{asset('images/candidato-placeholder.avif')}}" class="card-img-top" alt="Foto do Candidato">
+            <div class="card-body">
+                <h5 class="card-title font-weight-bold">Nome Do Candidato</h5>
+                <p class="card-text">Lorem Ipsum</p>
+                <a href="#" class="btn btn-primary">Ver Mais</a>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
