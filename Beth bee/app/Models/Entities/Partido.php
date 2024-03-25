bd16a23d175ba4c80c6c9650dc66c34172dc2629
rodrigo.santos <rodrigoos@dcomp.ufs.br>
2024-03-25 07:19:09 -0300

    candidato temp

--- Beth bee/app/Models/Entities/Partido.php
+++ Beth bee/app/Models/Entities/Partido.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Partido extends Model
 {
     use HasFactory;
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'partido';
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var string
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+     /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+
+    ];
 }
